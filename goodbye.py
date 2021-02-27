dcc216c053cc58ba7e85cda2415cbaecef4f3898
fowle22d <>
2021-02-27 14:17:44 -0500

    Initial commit

---
+++ goodbye.py
@@ -0,0 +1,8 @@
+"""
+A very simple goodbye world file.
+
+Change it to say goodbye to your name. 
+"""
+
+def goodbye():
+	print("goodbye world")
---
+++ hello.py
@@ -0,0 +1,11 @@
+"""
+A very simple hello world file.
+
+Change it to say hello to your name. 
+"""
+
+from goodbye import goodbye
+
+print("hello world")
+
+goodbye()
